fe1d68d9a1709c9100625714312e880ef5af8087
SalisZahid <44533323+SalisZahid@users.noreply.github.com>
2018-12-26 17:56:35 +0500

    Add files via upload
    Advantages of trees

---
+++ lectures/16/first.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>Advantages of Trees</h1>";
+
+$arr=Array("Provide Oxygen","Provide food","Protection and home for birds");
+
+for($i=0;$i<3;$i++)
+{
+	echo "<li>";
+	echo $arr[$i];
+	echo "</li>";
+
+}
+
+?>
